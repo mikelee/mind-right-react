5fd5a902dd8bea7dcd2f1ab2eaa755e9c9988d00
Mike <mike.lee816@gmail.com>
2021-10-28 18:19:55 -0400

    Get user from onAuthStateChanged()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+
 import './App.scss';
 
 import Homepage from './components/homepage/homepage.component';
 
+interface User {
+  email: string | null,
+  uid: string
+}
+
 function App() {
+	const [user, setUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		let unsubscribeFromAuth = onAuthStateChanged(auth, user => {
+			let currentUser;
+
+			if (user) {
+				currentUser = {
+					email: user.email,
+					uid: user.uid
+				}
+			} else {
+				currentUser = null;
+			}
+			
+			setUser(currentUser);
+		});
+
+		return unsubscribeFromAuth();
+	}, [auth]);
+
   return (
-    <div className="App">
-      <Homepage />
+  	<div className="App">
+    	{
+			!user
+			? <Homepage />
+			: <p>no user</p>
+      	}
     </div>
   );
 }
