c846b86aaf8a1cd6312b6e18ec8d3c5be4c16945
Mike <mike.lee816@gmail.com>
2021-10-28 15:23:28 -0400

    Add Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVNc_hcsOwjf8SbHvbvgs9MdPd7ZVyftQ',
+  authDomain: 'mind-right-d74a0.firebaseapp.com',
+  projectId: 'mind-right-d74a0',
+  storageBucket: 'mind-right-d74a0.appspot.com',
+  messagingSenderId: '553059270622',
+  appId: '1:553059270622:web:042404e32dffee986b674f',
+  measurementId: 'G-4J6LVW0T80'
+};
+
+const app = initializeApp(firebaseConfig);
