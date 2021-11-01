6427f8f2c84fda02c5b35c45fae7c2ffe28a9d8d
Mike <mike.lee816@gmail.com>
2021-11-01 13:37:39 -0400

    Change test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders MindRight text in Homepage component', () => {
   render(<App />);
   const title = screen.getByText(/MindRight/);
   expect(title).toBeInTheDocument();
