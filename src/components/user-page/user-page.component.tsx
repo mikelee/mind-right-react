693d2923de1e86aaa1fdbbad09e14f9e1ba4e1c7
Mike <mike.lee816@gmail.com>
2021-11-06 14:04:29 -0400

    Get user data from database and display thoughts list

--- src/components/user-page/user-page.component.tsx
+++ src/components/user-page/user-page.component.tsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 import './user-page.styles.scss';
 
-const UserPage = () => (
-    <div className='user-page'>
-        <p>User Page</p>
-    </div>
-);
+export interface Thought {
+    text: string,
+    id: string
+}
+
+interface Props {
+    user: any
+}
+
+const UserPage: React.FC<Props> = ({ user }) => {
+
+    const [thoughts, setThoughts] = useState<Thought[] | null>(null);
+
+    useEffect(() => {
+        const callGetUserData = async () => {
+            const data = await getUserData(user.uid);
+		    setThoughts(data);
+        }
+
+        callGetUserData();
+    }, [user.uid]);
+
+    const getUserData = async (uid: string) => {
+		const thoughts: any = [];
+
+		const usersRef = collection(db, 'users');
+		const userQuery = query(usersRef, where('uid', '==', uid));
+		const querySnapshot = await getDocs(userQuery);
+
+        const foundUser = querySnapshot.docs[0];
+        const thoughtsSnap = await getDocs(collection(db, `users/${foundUser.id}/thoughts`))
+
+        thoughtsSnap.forEach(el => {
+				const data = el.data();
+
+				const thought = {
+					...data,
+					id: el.id
+				}
+
+				thoughts.push(thought);
+			});
+
+            return thoughts;
+	}
+    
+    return (
+        <div className='user-page'>
+            <p>User Page</p>
+
+            {
+                thoughts?.map((thought: Thought) => <p key={thought.id}>{thought.text}</p>)
+            }
+        </div>
+    );
+};
 
 export default UserPage;
