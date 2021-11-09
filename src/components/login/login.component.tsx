8f56f572bedb4e8f07c137ea1ddb19ca1131c7b1
Mike <mike.lee816@gmail.com>
2021-11-09 15:14:53 -0500

    Change button to Button component

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -4,6 +4,8 @@ import { addNewUser, auth } from '../../firebase';
 
 import './login.styles.scss';
 
+import Button from '../button/button.component';
+
 interface Props {
     type: string | null
 }
@@ -70,7 +72,7 @@ const Login: React.FC<Props> = ({ type }) => {
             <form onSubmit={type === 'sign-up' ? signUp : signIn}>
                 <input name='email' type='text' placeholder='email' required />
                 <input name='password' type='password' placeholder='password' required />
-                <button type='submit'>{ type === 'sign-up' ? 'Sign Up' : 'Sign In'}</button>
+                <Button text='Sign In' type='submit' />
             </form>
         </div>
 )};
