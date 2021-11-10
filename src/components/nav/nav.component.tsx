fc9b0e74c3d2efb93a52ac43ea1fa0e12463f7fc
Mike <mike.lee816@gmail.com>
2021-11-10 18:31:18 -0500

    Add menu button to toggle menu

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
 
 import './nav.styles.scss';
 
+import Menu from '../menu/menu.component';
+
 const Nav = () => {
+
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    }
 
     const logout = () => {
         signOut(auth)
@@ -17,6 +25,12 @@ const Nav = () => {
 
     return (
         <div className='nav'>
+            {
+                menuVisible
+                ? <Menu />
+                : null
+            }
+            <div className='menu-button' onClick={toggleMenu} ></div>
             <button className='logout-button' onClick={logout}>Log Out</button>
         </div>
     );
