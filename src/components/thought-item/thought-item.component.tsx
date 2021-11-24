5f660f4843a693e02cbd8dacb302c45492d342dc
Mike <mike.lee816@gmail.com>
2021-11-24 17:29:14 -0500

    Edit thought when focus is taken off of input

--- src/components/thought-item/thought-item.component.tsx
+++ src/components/thought-item/thought-item.component.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { collection, doc, getDocs, updateDoc, query, where } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 import './thought-item.styles.scss';
 import { User } from '../../App';
@@ -10,10 +12,26 @@ interface Props {
     user: User
 }
 
-const ThoughtItem: React.FC<Props> = ({ text, image, id }) => {
+const ThoughtItem: React.FC<Props> = (props) => {
+    const { text, image, id, user } = props;
+
+    const editThought = async (e: React.FocusEvent<HTMLFormElement, Element>) => {
+        const { name, value } = e.target;
+
+        const usersRef = collection(db, 'users');
+		const userQuery = query(usersRef, where('uid', '==', user.uid));
+		const querySnapshot = await getDocs(userQuery);
+        const foundUser = querySnapshot.docs[0];
+
+        const thoughtRef = doc(db, `users/${foundUser.id}/thoughts`, id);
+
+        await updateDoc(thoughtRef, {
+            [name]: value
+          });
+    }
 
     return (
-    <form style={{backgroundImage: `url(${image})`}} className='thought-item'>
+    <form style={{backgroundImage: `url(${image})`}} className='thought-item' onBlur={(e) => editThought(e)} >
         <div className='label text'>
             <p>Text</p>
             <input name='text' defaultValue={text} className='thought-item-input' />
