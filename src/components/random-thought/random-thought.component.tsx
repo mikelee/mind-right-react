f2a90328ca2962566a81fcdbf30d87066a4f18a4
Mike Lee <mike.lee816@gmail.com>
2022-01-07 17:30:20 -0500

    Prevent the same thought from appearing twice in a row

--- src/components/random-thought/random-thought.component.tsx
+++ src/components/random-thought/random-thought.component.tsx
@@ -15,19 +15,23 @@ const RandomThought: React.FC<Props> = ({ thoughts, user }) => {
     const randomThoughtRef = useRef<any>(null);
 
     const [text, setText] = useState('');
+    const [thoughtId, setThoughtId] = useState('');
 
     useEffect(() => {
         shuffleThought(thoughts);
 
     }, [thoughts]);
 
-    const shuffleThought = async (thoughts: Thought[] | null) => {
+    const shuffleThought: any = async (thoughts: Thought[] | null) => {
         if (thoughts && thoughts.length > 0) {
             const randomIndex = Math.floor((Math.random() * thoughts.length));
 
             const thought = thoughts[randomIndex];
 
+            if (thought.id === thoughtId && thoughts.length > 1) return shuffleThought(thoughts);
+
             setText(thought.text);
+            setThoughtId(thought.id);
 
             randomThoughtRef.current.style.backgroundImage = `url(${thought.image})`;
         } else {
