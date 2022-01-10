bdae2d6b85bc2e54f1763b0fc2f5fed6b540de49
Mike Lee <mike.lee816@gmail.com>
2022-01-09 20:07:23 -0500

    Create Submenu component

---
+++ src/components/submenu/submenu.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './submenu.styles.scss';
+
+const Submenu: React.FC = () => {
+    return (
+        <div className='submenu'></div>
+    );
+}
+
+export default Submenu;
