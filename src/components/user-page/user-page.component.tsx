8ec46b43b1e736bbba51c91d4a5662d965453bd9
Mike Lee <mike.lee816@gmail.com>
2022-01-14 15:55:54 -0500

    Filter out thoughts that do not have a category that is selected and pass only the selected thoughts to RandomThought component

--- src/components/user-page/user-page.component.tsx
+++ src/components/user-page/user-page.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './user-page.styles.scss';
@@ -18,12 +18,47 @@ interface Props {
 }
 
 const UserPage: React.FC<Props> = ({ categories, thoughts, user, getUserData }) => {
+
+    const [selectedThoughts, setSelectedThoughts] = useState<Thought[] | null>(thoughts);
+
+    const updateSelectedThoughts = () => {
+        // if the category is selected, put its id in the selectedCategories array
+        let selectedCategories: string[] = [];
+        categories?.map(category => {
+            if (category.selected) {
+                selectedCategories.push(category.id);
+            };
+        });
+
+        // use all thoughts if no categories are selected
+        if (selectedCategories.length === 0) {
+            setSelectedThoughts(thoughts);
+            return;
+        }
+
+        // put all thoughts that have a category that is in the selectedCategories array in the updatedThoughts array
+        const updatedThoughts = thoughts?.filter(thought => {
+            let isTrue= false;
+
+            thought.categories.forEach(categoryId => {
+                if (selectedCategories?.includes(categoryId)) isTrue = true;
+            });
+
+            return isTrue;
+        });
+
+        if (updatedThoughts) setSelectedThoughts(updatedThoughts);
+    }
+
+    useEffect(() => {
+        updateSelectedThoughts();
+    }, [categories]);
     
     return (
         <div className='user-page'>
             <Nav />
             <Routes>
-                <Route path='/' element={<RandomThought thoughts={thoughts} />} />
+                <Route path='/' element={<RandomThought thoughts={selectedThoughts} />} />
                 <Route path='/thoughts' element={<ThoughtsList categories={categories} thoughts={thoughts} user={user} getUserData={getUserData} />} />
             </Routes>
         </div>
