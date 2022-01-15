ec498be0d968bf720215602ce9f46a8ba7bb1223
Mike Lee <mike.lee816@gmail.com>
2022-01-15 15:19:42 -0500

    Render Submenu component with Categories component when category menu-item is hovered on

--- src/components/menu/menu.component.tsx
+++ src/components/menu/menu.component.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase';
 
 import './menu.styles.scss';
 
+import Categories from '../categories/categories.component';
+import Submenu from '../submenu/submenu.component';
+
 interface Props {
     toggleMenu: () => void
 }
 
 const Menu: React.FC<Props> = ({ toggleMenu }) => {
+
+    const [categoriesVisible, setCategoriesVisible] = useState(false);
     
     let navigate = useNavigate();
+
+    const toggleCategories = () => {
+        setCategoriesVisible(!categoriesVisible);
+    }
     
     const logout = () => {
         signOut(auth)
@@ -27,6 +36,14 @@ const Menu: React.FC<Props> = ({ toggleMenu }) => {
         <div className='menu'>
             <Link to='/home' className='menu-item' onClick={toggleMenu}>Home</Link>
             <Link to='/home/thoughts' className='menu-item' onClick={toggleMenu}>Thoughts</Link>
+            <div className='menu-item menu-item--categories' onMouseEnter={toggleCategories} onMouseLeave={toggleCategories} >
+                Categories
+                {
+                    categoriesVisible
+                    ? <Submenu childComponent={<Categories categories={categories} user={user} getCategories={getCategories} />} />
+                    : null
+                }
+            </div>
             <div className='menu-item' onClick={logout}>Log Out</div>
         </div>
     );
