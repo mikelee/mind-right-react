e752523e002ce00be5a73a3394620b72e1e1e901
Mike Lee <mike.lee816@gmail.com>
2022-01-19 18:47:38 -0500

    Add button to add new categories

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { doc, updateDoc } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { doc, collection, updateDoc, addDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 import './categories.styles.scss';
@@ -8,6 +8,7 @@ import { Category } from '../data-loader/data-loader.component';
 import { User } from '../../App';
 
 import Checkbox from '../checkbox/checkbox.component';
+import Button from '../button/button.component';
 
 
 interface Props {
@@ -17,6 +18,26 @@ interface Props {
 }
 
 const Categories: React.FC<Props> = ({ categories, user, getCategories }) => {
+
+    const [displayAddInput, setDisplayAddInput] = useState(false);
+    const [newCategoryName, setNewCategoryName] = useState('');
+
+    const addCategory = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        if (user) {
+            const categoriesRef = collection(db, 'categories');
+    
+            await addDoc(categoriesRef, {
+                name: newCategoryName,
+                selected: false,
+                userId: user.uid
+            });
+
+            await getCategories(user.uid);
+            setDisplayAddInput(false);
+        }
+    }
 
     const toggleCategory = async (id: string, selected: boolean) => {
         const categoryRef = doc(db, 'categories', id);
@@ -28,14 +49,27 @@ const Categories: React.FC<Props> = ({ categories, user, getCategories }) => {
 
     return (
         <div className='categories'>
-            {
-                categories?.map(category =>
-                    <div key={category.id} className='category'>
-                        <Checkbox checked={category.selected} onClick={() => toggleCategory(category.id, category.selected)} />
-                        <p className='category-text'>{category.name}</p>
-                    </div>
-                )
-            }
+            <div className='categories-list'>
+                {
+                    categories?.map(category =>
+                        <div key={category.id} className='category'>
+                            <Checkbox checked={category.selected} onClick={() => toggleCategory(category.id, category.selected)} />
+                            <p className='category-text'>{category.name}</p>
+                        </div>
+                    )
+                }
+            </div>
+            <div className='add-section'>
+                {
+                    displayAddInput
+                    ? 
+                        <form>
+                            <input type='text' placeholder='Category name' onChange={(e) => setNewCategoryName(e.target.value)} />
+                            <Button text='Submit' onClick={(e: React.MouseEvent<HTMLButtonElement>) => addCategory(e)} />
+                        </form>
+                    : <Button className='add-button' text='Add' onClick={() => setDisplayAddInput(true)} />
+                }
+            </div>
         </div>
     );
 }
