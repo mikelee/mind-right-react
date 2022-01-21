a6458d6aa51bf3b53816b55beb81c1cf816789a3
Mike Lee <mike.lee816@gmail.com>
2022-01-21 17:26:12 -0500

    Double click category name to delete category

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { doc, collection, updateDoc, addDoc } from 'firebase/firestore';
+import { doc, collection, updateDoc, addDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 import './categories.styles.scss';
 
-import { Category } from '../data-loader/data-loader.component';
+import { Category, Thought } from '../data-loader/data-loader.component';
 import { User } from '../../App';
 
 import Checkbox from '../checkbox/checkbox.component';
@@ -13,14 +13,18 @@ import Button from '../button/button.component';
 
 interface Props {
     categories: Category[] | null,
+    thoughts: Thought[] | null,
     user: User | null,
-    getCategories: (uid: string) => Promise<any>
+    getCategories: (uid: string) => Promise<any>,
+    getUserData: (uid: string) => Promise<any>
 }
 
-const Categories: React.FC<Props> = ({ categories, user, getCategories }) => {
+const Categories: React.FC<Props> = ({ categories, thoughts, user, getCategories, getUserData }) => {
 
     const [displayAddInput, setDisplayAddInput] = useState(false);
     const [newCategoryName, setNewCategoryName] = useState('');
+    const [confirmDeleteVisible, setConfirmDeleteVisible] = useState(false);
+    const [categoryForDeletion, setCategoryForDeletion] = useState<Category | null>(null);
 
     const addCategory = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
@@ -47,6 +51,42 @@ const Categories: React.FC<Props> = ({ categories, user, getCategories }) => {
         if (user) getCategories(user.uid);
     }
 
+    const clickDelete = (category: Category) => {
+        setConfirmDeleteVisible(true);
+        setCategoryForDeletion(category);
+    }
+
+    const deleteCategory = async () => {
+        if (categoryForDeletion) {
+            const categoryRef = doc(db, 'categories', categoryForDeletion.id);
+
+            await deleteDoc(categoryRef);
+
+            // get thoughts that have the deleted category in their categories array
+            const thoughtsWithCategory = thoughts?.filter(thought => {
+                if (thought.categories.includes(categoryForDeletion.id)) return true;
+            });
+
+            // remove the deleted category from each thought's category array
+            thoughtsWithCategory?.forEach(thought => {
+                const categoryIndex = thought.categories.indexOf(categoryForDeletion.id);
+                thought.categories.splice(categoryIndex, 1);
+
+                const thoughtRef = doc(db, 'thoughts', thought.id);
+
+                updateDoc(thoughtRef, {
+                    categories: thought.categories
+                });
+            });
+
+
+            // update categories and thoughts
+            if (user) await getUserData(user.uid);
+            
+            setConfirmDeleteVisible(false);
+        }
+    }
+
     return (
         <div className='categories'>
             <div className='categories-list'>
@@ -54,7 +94,7 @@ const Categories: React.FC<Props> = ({ categories, user, getCategories }) => {
                     categories?.map(category =>
                         <div key={category.id} className='category'>
                             <Checkbox checked={category.selected} onClick={() => toggleCategory(category.id, category.selected)} />
-                            <p className='category-text'>{category.name}</p>
+                            <p className='category-text' onDoubleClick={() => clickDelete(category)} >{category.name}</p>
                         </div>
                     )
                 }
@@ -70,6 +110,17 @@ const Categories: React.FC<Props> = ({ categories, user, getCategories }) => {
                     : <Button className='add-button' text='Add' onClick={() => setDisplayAddInput(true)} />
                 }
             </div>
+            {
+                confirmDeleteVisible && categoryForDeletion
+                ?
+                    <div className='confirm-delete'>
+                        <p>{`Are you sure you want to delete ${categoryForDeletion.name}?`}</p>
+                        <button onClick={() => setConfirmDeleteVisible(false)}>Cancel</button>
+                        <button onClick={() => deleteCategory()}>Delete</button>
+                    </div>
+                : null
+            }
+            
         </div>
     );
 }
