3698250a6dfe8c0012ab32ae634b1e90d4a38591
Mike Lee <mike.lee816@gmail.com>
2022-04-01 19:27:25 -0400

    Add login test

---
+++ cypress/integration/login.spec.ts
@@ -0,0 +1,22 @@
+it('should log in user, display skeleton screen, and then display shuffle button', () => {
+    const username = Cypress.env('username');
+    const password = Cypress.env('password');
+
+    cy.visit('http://localhost:3000/');
+
+    cy.contains(/sign in/i).click();
+
+    cy.get('[name="email"]')
+        .type(username)
+        .should('have.value', username);
+
+    cy.get('[name="password"]')
+        .type(password, {log: false})
+        .should('have.value', password);
+
+    cy.get('form > .button').click();
+
+    cy.get('[data-testid="skeleton-screen"]').should('be.visible');
+
+    cy.contains(/shuffle/i);
+});
