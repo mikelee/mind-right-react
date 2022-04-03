b4b2a98bcbebeb0b436f9d5bc9472f44b5210157
Mike Lee <mike.lee816@gmail.com>
2022-04-03 18:14:24 -0400

    Add test

---
+++ cypress/integration/shuffle.spec.ts
@@ -0,0 +1,19 @@
+beforeEach(() => {
+    const uid = 'EaEu8zAupdXJPWKtCcpKhGJP8Bi1';
+    cy.login(uid);
+    cy.visit('http://localhost:3000/home');
+});
+
+afterEach(() => {
+    cy.logout();
+});
+
+it('should click shuffle button and change the text with the new thoughts text', () => {
+    cy.get('[data-testid="random-thought-text"]').then(randomThoughtText => {
+        const text = randomThoughtText.text();
+
+        cy.get('button').click();
+
+        cy.get('[data-testid="random-thought-text"]').should('not.have.text', text);
+    });
+});
