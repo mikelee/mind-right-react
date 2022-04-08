d12229f8b3c6501540ab272f50a46c6f719490e6
Mike Lee <mike.lee816@gmail.com>
2022-04-08 14:36:36 -0400

    Create tests for message when there are no thoughts and when there are no thoughts with matching categories. Remove beforeEach() function

--- cypress/integration/shuffle.spec.ts
+++ cypress/integration/shuffle.spec.ts
@@ -1,6 +1,10 @@
-import { deleteTestData, populateTestData } from '../databaseSetUp';
+import { deleteTestData, populateTestData, populateTestDataNoThoughts } from '../databaseSetUp';
 
-beforeEach(() => {
+afterEach(() => {
+    cy.logout();
+});
+
+it('should click shuffle button and change the text with the new thoughts text', () => {
     const uid = 'PsRL0Nqd20fcPYDHrzsOV9yF9j62';
 
     deleteTestData();
@@ -8,13 +12,7 @@ beforeEach(() => {
 
     cy.login(uid);
     cy.visit('http://localhost:3000/home');
-});
 
-afterEach(() => {
-    cy.logout();
-});
-
-it('should click shuffle button and change the text with the new thoughts text', () => {
     cy.get('[data-testid="random-thought-text"]').then(randomThoughtText => {
         const text = randomThoughtText.text();
 
@@ -22,4 +20,36 @@ it('should click shuffle button and change the text with the new thoughts text',
 
         cy.get('[data-testid="random-thought-text"]').should('not.have.text', text);
     });
+});
+
+it('should display message for no thoughts in database', () => {
+    const uid = 'PsRL0Nqd20fcPYDHrzsOV9yF9j62';
+
+    deleteTestData();
+    populateTestDataNoThoughts();
+
+    cy.login(uid);
+    cy.visit('http://localhost:3000/home');
+
+    // no thoughts in the database
+    cy.contains('Add some thoughts and get started!');
+});
+
+it('should display message for no thoughts with the selected category', () => {
+    const uid = 'PsRL0Nqd20fcPYDHrzsOV9yF9j62';
+
+    deleteTestData();
+    populateTestData();
+
+    cy.login(uid);
+    cy.visit('http://localhost:3000/home');
+
+    // check first category
+    cy.get('.menu-button').click();
+    cy.get('.menu-item--categories').realHover();
+    cy.get(':nth-child(1) > [data-testid="checkbox"]').realClick();
+    cy.get('.menu-button').click();
+
+    // no thoughts have the first category as a thoughtCategory
+    cy.contains('No thoughts have the selected categories');
 });
