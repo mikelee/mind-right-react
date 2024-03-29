9646593c5f2d11a89fa6f677b50cb930327198e5
Mike Lee <mike.lee816@gmail.com>
2024-03-29 16:14:46 -0400

    Add service account credential to firebase-admin
    Now the GOOGLE_APPLICATION_CREDENTIALS variable does not need to be set to the service account file when running cypress.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import admin from 'firebase-admin';
 import { defineConfig } from 'cypress';
 import { plugin as cypressFirebasePlugin } from 'cypress-firebase';
+import firebaseAccountCredentials from './serviceAccount.json';
+
+const serviceAccount = firebaseAccountCredentials as admin.ServiceAccount;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 export default defineConfig({
   e2e: {
